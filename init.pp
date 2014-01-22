a6f35f22178edf75d517793983baa97dd97fca83
Richard Sentino <richard@mingdinative.com>
2014-01-22 23:47:55 +1300

    Allowed port 80 and 443 in IPTables

--- init.pp
+++ init.pp
@@ -1,24 +1,3 @@
-/*
- *
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
-*/
-
 
 ###############################################################################
 # Defaults
@@ -33,6 +12,32 @@ file { "/vagrant_www/home":
 
 
 ###############################################################################
+# Apache
+###############################################################################
+
+class { 'apache':
+  mpm_module => 'prefork',
+}
+
+include 'apache::mod::php'
+
+apache::vhost { 'home.pushpath.dev':
+    port => '80',
+    docroot => '/vagrant_www/home'
+}
+
+###############################################################################
+# PHP
+###############################################################################
+
+class { 'php': }
+
+php::module { "pgsql": }
+php::module { "pdo": }
+php::module { "common": }
+php::module { "devel": }
+
+###############################################################################
 # IPTables
 ###############################################################################
 resources { "firewall":
@@ -42,18 +47,7 @@ resources { "firewall":
 class { 'firewall': }
 
 firewall { '100 allow http and https access':
-    port   => [80, 443],
-    proto  => tcp,
-    action => accept,
-}
-
-###############################################################################
-# Apache
-###############################################################################
-
-class { 'apache': }
-
-apache::vhost { 'home.pushpath.dev':
-    port => '80',
-    docroot => '/vagrant_www/home'
+  port   => [80, 443],
+  proto  => tcp,
+  action => accept,
 }
